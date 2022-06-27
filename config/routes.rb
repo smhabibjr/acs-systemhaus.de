5df6f2814862dc0a46d48c0a851103ad4b302838
HabibJR <smhabib.abir2030@gmail.com>
2022-06-27 13:09:03 +0200

    [T3] login and sign up page done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'signup' => "sign_up#index"
+  get 'login' => "login#index"
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
